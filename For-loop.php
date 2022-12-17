87392c3ab5f28daac4357ab448c1ce62be00db2e
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2022-12-17 12:53:46 +0600

    code

---
+++ For-loop.php
@@ -0,0 +1,13 @@
+<?php 
+    
+    for($i = 1; $i <= 10; $i++)
+    {
+        echo "Value of ". $i . "\n";
+        
+    }
+    ///Step 1:  $i = 1 -> is the loop initialization (it is initialized only once) 
+    ///Step 2: $i <= 10 -> is the loop condition (how many times the loop will execute)
+    ///Step 3: echo "Value of ". $i . "\n"; -> this will execute
+    ///Step 4: $i++ -> loop value ($i) will increase 
+
+?>
